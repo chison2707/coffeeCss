71a8d639b9851d6c9d1728e3f2fc76d6f845570c
chison2707 <chiphansonzz17@gmail.com>
2025-06-21 09:19:33 +0700

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
     content: ["./public/**/*.{html,js}"],
     theme: {
         extend: {
+            fontFamily: {
+                Karla: [
+                    'Karla',
+                    'sans-serif'
+                ]
+            },
             colors: {
                 'light-coffee': '#C3855D'
             }
